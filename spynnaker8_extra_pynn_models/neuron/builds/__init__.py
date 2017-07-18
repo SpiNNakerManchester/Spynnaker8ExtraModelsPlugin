87adae4557ec10577596d16b56b1a4acfb161399
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2017-07-18 12:42:27 +0100

    Moved to sPyNNaker8

--- spynnaker8_extra_pynn_models/neuron/builds/__init__.py
+++
@@ -1,7 +0,0 @@
-from .if_cond_exp_stoc_data_holder import IfCondExpStocDataHolder
-from .if_curr_delta_data_holder import IfCurrDeltaDataHolder
-from .if_curr_exp_ca2_adaptive_data_holder \
-    import IfCurrExpCa2AdaptiveDataHolder
-
-__all__ = ["IfCondExpStocDataHolder", "IfCurrDeltaDataHolder",
-           "IfCurrExpCa2AdaptiveDataHolder"]
